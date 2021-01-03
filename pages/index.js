034ad3cb64083352ea23eea0b83c5e8acb6c8dab
Austin Condiff <austin.condiff@gmail.com>
2021-01-03 14:47:22 -0600

    New favicon

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ file, preview }) {
     <div className="container">
       <Head>
         <title>{page.title}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <InlineForm form={form}>
         <main>
