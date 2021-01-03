68ef2bd6c41268a91a61fca5e6d3ca69545e36ac
Austin Condiff <austin.condiff@gmail.com>
2021-01-02 22:22:21 -0600

    Added TinaCMS

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,50 @@
 import '@styles/globals.css'
+import { withTina } from 'tinacms'
+import { GithubClient, TinacmsGithubProvider, GithubMediaStore } from 'react-tinacms-github'
+import { MarkdownFieldPlugin } from 'react-tinacms-editor'
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const github = new GithubClient({
+  proxy: '/api/proxy-github',
+  authCallbackRoute: '/api/create-github-access-token',
+  clientId: process.env.GITHUB_CLIENT_ID,
+  baseRepoFullName: process.env.REPO_FULL_NAME,
+  baseBranch: process.env.BASE_BRANCH,
+})
+
+const onLogin = async () => {
+  const token = localStorage.getItem('tinacms-github-token') || null
+  const headers = new Headers()
+
+  if (token) {
+    headers.append('Authorization', 'Bearer ' + token)
+  }
+
+  const resp = await fetch(`/api/preview`, { headers: headers })
+  const data = await resp.json()
+
+  if (resp.status == 200) window.location.href = window.location.pathname
+  else throw new Error(data.message)
 }
 
-export default Application
+const onLogout = () => {
+  return fetch(`/api/reset-preview`).then(() => {
+    window.location.reload()
+  })
+}
+
+function Application({ Component, pageProps }) {
+  return (
+    <TinacmsGithubProvider onLogin={onLogin} onLogout={onLogout} error={pageProps.error}>
+      <Component {...pageProps} />
+    </TinacmsGithubProvider>
+  )
+}
+
+export default withTina(Application, {
+  enabled: true,
+  sidebar: false,
+  toolbar: true,
+  plugins: [MarkdownFieldPlugin],
+  apis: { github },
+  media: new GithubMediaStore(github),
+})
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,63 @@
 import Head from 'next/head'
+import { usePlugin } from 'tinacms'
+import { InlineForm, InlineTextarea } from 'react-tinacms-inline'
+import { useGithubJsonForm, useGithubToolbarPlugins } from 'react-tinacms-github'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export default function Home({ jsonFile }) {
+  const formConfig = {
+    fields: [
+      {
+        name: 'title',
+        label: 'Title',
+        component: 'text',
+      },
+      {
+        name: 'body',
+        label: 'Body',
+        component: 'textarea',
+      },
+    ],
+  }
+
+  console.log({ jsonFile })
+
+  const [page, form] = useGithubJsonForm(jsonFile, formConfig)
+
+  usePlugin(form)
+  useGithubToolbarPlugins()
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>{page.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
+      <InlineForm form={form}>
+        <main>
+          <h1>
+            <InlineTextarea name={page.title} />
+          </h1>
+          <div className="description">
+            <InlineTextarea name={page.body} />
+          </div>
+        </main>
+      </InlineForm>
       <Footer />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const content = await import(`../content/index.json`)
+
+  return {
+    props: {
+      jsonFile: {
+        fileRelativePath: `/content/index.json`,
+        data: content.default,
+      },
+    },
+  }
 }
