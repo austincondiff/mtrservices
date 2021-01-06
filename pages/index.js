17e40548e0571c2edf281e43a68c931d1ecff3bf
Austin Condiff <austin.condiff@gmail.com>
2021-01-05 22:24:45 -0600

    Adding navigation json file

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
 import { useGithubJsonForm, useGithubToolbarPlugins } from 'react-tinacms-github'
 import { InlineForm, InlineBlocks, InlineTextarea } from 'react-tinacms-inline'
-import { usePlugin } from 'tinacms'
+import { useFormScreenPlugin, usePlugin } from 'tinacms'
 import Footer from '@components/common/Footer'
 import { featureListBlock } from '@components/blocks/FeatureList'
 import { heroBlock } from '@components/blocks/Hero'
 import { paragraphBlock } from '@components/blocks/Paragraph'
 
-export default function Home({ file, preview }) {
+export default function Home({ file, navFile, preview }) {
   const formOptions = {
     label: 'Home Page',
     fields: [
@@ -16,7 +16,16 @@ export default function Home({ file, preview }) {
       { name: 'body', component: 'textarea' },
     ],
   }
+  const navFormOptions = {
+    label: 'Navigation',
+    fields: [
+      { name: 'logo', component: 'media' },
+      { name: 'test', component: 'textarea' },
+    ],
+  }
   const [page, form] = useGithubJsonForm(file, formOptions)
+  const [nav, navForm] = useGithubJsonForm(navFile, navFormOptions)
+  useFormScreenPlugin(navForm)
   usePlugin(form)
   useGithubToolbarPlugins()
 
@@ -51,6 +60,10 @@ export const getStaticProps = async function ({ preview, previewData }) {
       file: {
         fileRelativePath: 'content/index.json',
         data: (await import('../content/index.json')).default,
+      },
+      navFile: {
+        fileRelativePath: 'content/navigation.json',
+        data: (await import('../content/navigation.json')).default,
       },
     },
   }
